400ca625f8434947640a8738cf5516365b8036e0
Tseyko Roman <tseyko.roman@mail.ru>
2020-10-09 13:03:23 +0300

    Ответы на задания для урока №1
    Ответы на задания для урока №1

---
+++ 1.py
@@ -0,0 +1,15 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+name = "Вася"
+print("Меня зовут", name)
+
+number = int(input('Введите целое число от 0 до 100: '))
+while number < 100:
+    print(number)
+    number = number + 1
+print('программа завершена успешно')
+
+name = input("Enter your name: ")
+print_hi(name)
